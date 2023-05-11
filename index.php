c37e921e359f15eb8cf4cd977ede8cde4ece08ae
Moparus <mzpotoniec@gmail.com>
2023-05-11 21:42:40 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="3">
+    <title>Strona Testowa</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body class="bg-black">
+    <div class="flex mx-auto max-w-s items-center justify-center">
+        <h1 class="text-white justify-center m-10 font-bold">
+            Witam na ładnej stronie
+        </h1>
+        <button class="justify-center text-white m-10 font-bold">Przycisk</button>
+    </div>
+</body>
+</html>
+<?php
